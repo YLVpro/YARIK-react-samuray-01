b7570cc5f819f5303cb39c7ea7c274c801df12d1
YLVpro <YLVpro@gmail.com>
2021-03-06 18:44:30 +0200

    29. Уроки React JS (упаковываем данные в state) - react

---
+++ src/Redux/State.js
@@ -0,0 +1,29 @@
+
+  let state = {
+      profilePage: {
+        posts: [
+            { id: 1, message: 'Hi, how are you?', likesCount: 23 },
+             { id: 2, message: 'All is well', likesCount: 16 } 
+            ]   
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Vasya' },
+                { id: 2, name: 'Petya' },
+                { id: 3, name: 'Kolya' },
+                { id: 4, name: 'Sanya' },
+                { id: 5, name: 'Vetal' }
+            ],
+            messagesPage: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'It-man' },
+                { id: 3, message: 'USA' },
+                { id: 4, message: 'USA' },
+                { id: 5, message: 'USA' }
+            ] 
+        }
+    }
+
+
+
+    export default state;
